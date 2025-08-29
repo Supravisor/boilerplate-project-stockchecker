d1d555b1feb0a1442c7e1477e819ec5df7982c88
Supravisor Supravisor <webtraininghashtag@outlook.com>
2025-08-29 05:21:13 +0000

    add likes variable

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,17 @@
 'use strict';
 
 module.exports = function (app) {
+  let likes = 0;
 
   app.route('/api/stock-prices')
     .get(function (req, res){
       let stock = req.query.stock;
-      let like = req.query.like ? 1 : 0;
+      let like = req.query.like;
+console.log(stock, like, likes);
+console.log(like === 1);
+      if (like == 1) {
+        likes++;
+      }
       const getStock = async (selection) => {
         try {
           if (typeof(stock) === "string") {
@@ -13,7 +19,7 @@ module.exports = function (app) {
             const info = await data.json();
             const { change,changePercent,close,high,latestPrice,latestTime,latestVolume,low,open,previousClose,symbol,volume } = info;
 
-            return res.json({ "stockData": { "stock": stock, "price": latestPrice, "likes": like } });
+            return res.json({ "stockData": { "stock": stock, "price": latestPrice, "likes": likes } });
           } else {
           }
         } catch (err) {
