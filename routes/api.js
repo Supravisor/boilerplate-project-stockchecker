54583d47f31c09b513c166614d153b06608107ae
Supravisor Supravisor <webtraininghashtag@outlook.com>
2025-08-29 02:07:16 +0000

    refactor like variable

--- routes/api.js
+++ routes/api.js
@@ -5,15 +5,14 @@ module.exports = function (app) {
   app.route('/api/stock-prices')
     .get(function (req, res){
       let stock = req.query.stock;
-      let like = req.query.like;
-console.log(stock, like, Object.keys(req.query))
+      let like = req.query.like ? 1 : 0;
       const getStock = async (selection) => {
         try {
           const data = await fetch(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${selection}/quote`);
           const info = await data.json();
           const { change,changePercent,close,high,latestPrice,latestTime,latestVolume,low,open,previousClose,symbol,volume } = info;
 
-          return res.json({"stockData": { "stock": stock, "price": latestPrice, "likes": like } });
+          return res.json({ "stockData": { "stock": stock, "price": latestPrice, "likes": like } });
         } catch (err) {
             console.log(err);
         }
