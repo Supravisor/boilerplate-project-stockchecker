4baea32a07d8d77795f4f84106ea9bddbd7cffd4
Supravisor Supravisor <webtraininghashtag@outlook.com>
2025-09-03 05:06:29 +0000

    Viewing the same stock and liking it again

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,6 +5,7 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
+let stockLikes = 0;
 suite('Functional Tests', function() {
 
     test("Viewing one stock", function(done) {
@@ -19,11 +20,29 @@ suite('Functional Tests', function() {
           assert.property(stockData, 'price');
           assert.property(stockData, 'likes');
           assert.equal(stockData.stock, 'GOOG');
+        });
+        done();
+    });
+
+    test("Viewing one stock and liking it", function(done) {
+      chai
+        .request(server)
+        .get('/api/stock-prices')
+        .query({ stock: 'GOOG', like: true })
+        .end(function(err, res) {
+          const { stockData } = res.body;
+          stockLikes = stockData.likes
+          assert.equal(res.status, 200);
+          assert.property(stockData, 'stock');
+          assert.property(stockData, 'price');
+          assert.property(stockData, 'likes');
+          assert.equal(stockData.stock, 'GOOG');
+          assert.isAbove(stockData.likes, 0);
           done();
         });
     });
 
-    test("Viewing one stock and liking it", function(done) {
+    test("Viewing the same stock and liking it again", function(done) {
       chai
         .request(server)
         .get('/api/stock-prices')
@@ -35,9 +54,9 @@ suite('Functional Tests', function() {
           assert.property(stockData, 'price');
           assert.property(stockData, 'likes');
           assert.equal(stockData.stock, 'GOOG');
-          assert.isAbove(stockData.likes, 0);
-          done();
+          assert.equal(stockData.likes, stockLikes);
         });
+        done();
     });
 
 });
